f2656d3f81ef3d185177d9b6e5455e56d17b19ea
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-09-09 19:59:26 +0600

    Updated variable name

--- frontend/app/store/dynamic-base-query.tsx
+++ frontend/app/store/dynamic-base-query.tsx
@@ -3,12 +3,12 @@ import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const DynamicBaseQuery = async (args: any, api: any, extraOptions: any) => {
     const session = await getSession();
-    let headers = {};
-    if (session?.access) {
-        headers = {Authorization: `Bearer ${session.access}`};
-    }
+    // let headers = {};
+    // if (session?.access) {
+    //     headers = {Authorization: `Bearer ${session.access}`};
+    // }
     return fetchBaseQuery({
-        baseUrl: `${process.env.BACKEND_BASE_URL}`,
+        baseUrl: `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}`,
         prepareHeaders: (headers) => {
             if (session?.access) {
                 headers.set('Authorization', `Bearer ${session.access}`);
