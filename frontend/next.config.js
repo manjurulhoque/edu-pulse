6bde3bcd1f3b54232787828716c0c840b9956982
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-09-30 21:24:17 +0600

    Revert to image domains

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
         },
     },
     env: {
-        BACKEND_BASE_URL: process.env.BACKEND_BASE_URL,
+        BACKEND_BASE_URL: process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
         // NEXT_PUBLIC_BACKEND_BASE_URL: "http://localhost:8080",
         NEXT_PUBLIC_BACKEND_BASE_URL: process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
         BASE_URL: "http://localhost:3000",
