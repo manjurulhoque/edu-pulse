816118d945bffbd953e61ae973c8ac72947cb340
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-10-01 17:23:15 +0600

    Fixed env

--- frontend/app/actions/getVariables.ts
+++
@@ -1,6 +0,0 @@
-export async function getVariables() {
-    return {
-        BACKEND_BASE_URL: process.env.BACKEND_BASE_URL,
-        BASE_URL: process.env.BASE_URL,
-    };
-}
--- frontend/app/store/dynamic-base-query.tsx
+++ frontend/app/store/dynamic-base-query.tsx
@@ -1,20 +1,9 @@
 import { getSession } from "next-auth/react";
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import {getVariables} from "@/app/actions/getVariables";
 
 const DynamicBaseQuery = async (args: any, api: any, extraOptions: any) => {
     const session = await getSession();
-    let variables = await getVariables();
-    console.log(variables);
-    console.log(process.env.BACKEND_BASE_URL);
-    console.log(process.env.NEXT_PUBLIC_BACKEND_BASE_URL);
-    let BACKEND_BASE_URL: string;
-    if (typeof window === "undefined") {
-        BACKEND_BASE_URL = process.env.BACKEND_BASE_URL;
-    } else {
-        BACKEND_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_BASE_URL;
-    }
-    BACKEND_BASE_URL = variables.BACKEND_BASE_URL;
+    const  BACKEND_BASE_URL = process.env.BACKEND_BASE_URL;
     return fetchBaseQuery({
         baseUrl: `${BACKEND_BASE_URL}`,
         prepareHeaders: (headers) => {
