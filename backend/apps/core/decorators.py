0ef67101815ba31be8a2d6f7b0e95a352cb6651b
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2025-05-03 19:06:08 +0600

    feat: use status codes from fastapi for authentication error handling

--- backend/apps/core/decorators.py
+++ backend/apps/core/decorators.py
@@ -1,4 +1,4 @@
-from fastapi import HTTPException, Depends
+from fastapi import HTTPException, Depends, status
 from apps.users.helpers import get_current_user
 from apps.users.models import User
 
@@ -6,7 +6,7 @@ from apps.users.models import User
 def auth_required(current_user: User = Depends(get_current_user)):
     if not current_user:
         raise HTTPException(
-            status_code=401,
+            status_code=status.HTTP_401_UNAUTHORIZED,
             detail="User not authenticated"
         )
     return current_user
