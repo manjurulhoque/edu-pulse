04902d4353d3bef97234b3daad5cd28319b51397
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2025-05-09 23:52:35 +0600

    ✨ feat: implement pagination and user data handling in admin courses endpoint

--- backend/apps/core/admin_routers.py
+++ backend/apps/core/admin_routers.py
@@ -1,14 +1,50 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 from conf.database import get_db
 from apps.users.models import User
 from apps.core.decorators import admin_required
+from utils.response_utils import create_response, create_paginated_response
+from starlette import status
+from apps.courses import models as course_models
+from utils.params import common_parameters
 
 admin_router = APIRouter(prefix="/admin", tags=["admin"])
 
 
 @admin_router.get("/courses")
 async def get_all_courses(
-    db: Session = Depends(get_db), current_user: User = Depends(admin_required)
+    db: Session = Depends(get_db),
+    current_user: User = Depends(admin_required),
+    params: dict = Depends(common_parameters),
 ):
-    return {"message": "Admin courses"}
+    query = db.query(course_models.Course)
+    total = query.count()
+    page = params.get("page", 1)
+    page_size = params.get("page_size", 10)
+    skip = (page - 1) * page_size
+    courses = (
+        query.options(
+            joinedload(course_models.Course.user),
+            joinedload(course_models.Course.category),
+        )
+        .offset(skip)
+        .limit(params["page_size"])
+        .all()
+    )
+    for course in courses:
+        try:
+            if hasattr(course.user, "password"):
+                del course.user.password
+        except:
+            pass
+    return create_response(
+        data=create_paginated_response(
+            data=courses,
+            total=total,
+            page=params["page"],
+            page_size=params["page_size"],
+            path="/admin/courses",
+        ),
+        message="Courses fetched successfully",
+        status_code=status.HTTP_200_OK,
+    )
