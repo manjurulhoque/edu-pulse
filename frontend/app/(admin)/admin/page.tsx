a9e8ed32e5b9e02e2fb945cd42279b23ce585291
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2025-05-10 13:21:52 +0600

    ✨ feat(admin): add metadata for Admin Dashboard page

--- frontend/app/(admin)/admin/page.tsx
+++ frontend/app/(admin)/admin/page.tsx
@@ -1,9 +1,11 @@
-"use client";
-
 import AdminDashboardHome from "@/app/components/admin/AdminDashboardHome";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Admin Dashboard",
+    description: "Admin Dashboard",
+};
 
 export default function AdminDashboardPage() {
-    return (
-        <AdminDashboardHome />
-    );
+    return <AdminDashboardHome />;
 }
