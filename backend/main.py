84852bec8789ae26b2c373b9073daaeea677b978
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2025-05-18 01:03:18 +0600

    ✨ chore(main): import review_models in main.py
    - Added import statement for review_models from apps.reviews to main.py to support review-related functionalities.

--- backend/main.py
+++ backend/main.py
@@ -12,6 +12,7 @@ from apps.lessons import models as lesson_models
 from apps.categories import models as categories_models
 from apps.cart import models as cart_models
 from apps.checkout import models as checkout_models
+from apps.reviews import models as review_models
 from apps.users import routers as user_routers
 from apps.courses import routers as course_routers
 from apps.categories import routers as categories_routers
