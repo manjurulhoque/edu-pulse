c8ff2bd0914c00d3b584ecc96c97e53af366a535
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2025-05-22 19:41:17 +0600

    feat(wishlist): add metadata for wishlist page

--- frontend/app/(student)/student/dashboard/wishlist/page.tsx
+++ frontend/app/(student)/student/dashboard/wishlist/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Wishlist | EduPulse - Professional LMS Online Education Course",
+};
+
 const WishlistPage = () => {
     return <div>Wishlist</div>;
 };
