006e484ec340393faefd6844570c4b9184cef17b
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2025-05-24 00:15:17 +0600

    feat(course): generate dynamic metadata for course pages
    Add functionality to fetch course details and generate
    dynamic metadata including title and description based
    on the course information. This improves SEO and user
    experience by providing relevant content for each course.

--- frontend/app/(app)/courses/[slug]/page.tsx
+++ frontend/app/(app)/courses/[slug]/page.tsx
@@ -1,11 +1,21 @@
 import Header from "@/app/components/layout/Header";
-import { Metadata } from "next";
 import PageLinks from "@/app/components/common/PageLinks";
 import CourseDetails from "@/app/components/courses/CourseDetails";
+import { getCourseDetails } from "@/app/actions/getSingleCourse";
 
-export const metadata: Metadata = {
-    title: "Course page",
-    description: "Course list page",
+export const generateMetadata = async ({ params }: { params: { slug: string } }) => {
+    try {
+        const course = await getCourseDetails(params.slug);
+        return {
+            title: `${course?.title} | EduPulse - Professional LMS Online Education Course`,
+            description: course?.description,
+        };
+    } catch (error) {
+        return {
+            title: "Course not found",
+            description: "Course not found",
+        };
+    }
 };
 
 const CourseDetailsPage = ({ params }: { params: { slug: string } }) => {
